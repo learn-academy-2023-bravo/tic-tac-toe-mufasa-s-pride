0c984a96fd92dbf277b089d44ee29e74fe738f67
FLopez73 <flopez730@gmail.com>
2023-04-04 11:04:20 -0700

    driver switch

--- src/App.js
+++ src/App.js
@@ -5,46 +5,60 @@ import "./App.css"
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState("X")
-  // const [winner, setWinner] = useState(null)
-  
-  const handleClick = (i)=> {
-    if (squares[i] !== null) {
-      return   }
+  const [winner, setWinner] = useState(null)
+
+  const handleClick = (i) => {
+    if (squares[i] !== null || calculateWinner(squares)) {
+      return
+    }
     let newSquares = [...squares]
     newSquares[i] = turn
     setSquares(newSquares)
-    // setWinner(calculateWinner(newSquares))
-    newSquares[i] = turn === "X" ? "O" : "X";
-    
-    setSquares(newSquares);
-    setTurn(newSquares[i])
-     console.log(i)
-    }
-    function calculateWinner(squares) {
+    setWinner(calculateWinner(newSquares))
+    newSquares[i] = turn === "X" ? "O" : "X"
 
-      const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ]
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if (
-          squares[a] &&
-          squares[a] === squares[b] &&
-          squares[a] === squares[c]
-          
-        ) {
-          return squares[a]
-        }
+    setSquares(newSquares)
+    setTurn(newSquares[i])
+    console.log(i)
+  }
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a]
       }
-      return null
+      if (squares.every((squares) => squares !== null)) {
+        return "No player"
+      }
     }
+    return null
+  }
+
+  const champion = calculateWinner(squares)
+  let status
+  if (champion) {
+    status = `${champion} Wins!`
+    alert(status)
+  } else {
+    status = { turn }
+  }
+  const gameRestart = () => {
+    window.location.reload()
+  }
   return (
     <>
       <h1>Tic Tac Toe</h1>
@@ -59,6 +73,11 @@ const App = () => {
             />
           )
         })}
+      </div>
+      <div>
+        <button className="button" onClick={gameRestart}>
+          Play Again
+        </button>
       </div>
     </>
   )

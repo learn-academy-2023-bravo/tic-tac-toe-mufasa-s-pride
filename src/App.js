245bbe21ca726021bd4820f9265dd579f682a145
gnostertag <ostertag2000@gmail.com>
2023-04-04 10:14:23 -0700

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -5,14 +5,46 @@ import "./App.css"
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState("X")
-
+  // const [winner, setWinner] = useState(null)
   
   const handleClick = (i)=> {
-    // console.log(i)
+    if (squares[i] !== null) {
+      return   }
     let newSquares = [...squares]
-     newSquares[i] = turn
+    newSquares[i] = turn
     setSquares(newSquares)
-  }
+    // setWinner(calculateWinner(newSquares))
+    newSquares[i] = turn === "X" ? "O" : "X";
+    
+    setSquares(newSquares);
+    setTurn(newSquares[i])
+     console.log(i)
+    }
+    function calculateWinner(squares) {
+
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (
+          squares[a] &&
+          squares[a] === squares[b] &&
+          squares[a] === squares[c]
+          
+        ) {
+          return squares[a]
+        }
+      }
+      return null
+    }
   return (
     <>
       <h1>Tic Tac Toe</h1>
